d586c3419536ee3b90334b7a812fa771616affd8
josiah113366-sketch <josiah113366@gmail.com>
2026-08-28 13:35:12 +0900

    Feat: 변수 세팅할 항목 설정

--- infra/variables.tf
+++ infra/variables.tf
@@ -13,9 +13,17 @@ variable "project_name" {
 # opensearch 서비스(<- 엘라스틱서치)/ opensearch 대시보드(<- 키바나) 접속 가능한 IP 입력
 variable "allowed_cidr" {
   description = "opensearch 대시보드/API에 접속한 공인 IP x.x.x.x/32"
-  type = string
+  type        = string
+  default     = "222.108.125.33/32"
+  # 접속 위치가 바뀌면 접근 x -> ip를 변경하여 인프라 반영시켜야 함 
   validation {
-    condition = can(cidrhost(var.allowed_cidr, 0))
+    condition     = can(cidrhost(var.allowed_cidr, 0))
     error_message = "가능한 주소는 CIDR 형식이어야 합니다."
   }
 }
+
+# opensearch, spec(버전, 인스턴스 유형, 볼륨 단위, 인덱스 등 설정)
+
+# firehose 이름, firehose -> opensearch : iam role name
+
+# vector -> firehose : iam role name
