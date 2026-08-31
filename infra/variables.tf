d3eff276604aa936fdd99e40255537775e197cb0
josiah113366-sketch <josiah113366@gmail.com>
2026-08-31 10:27:52 +0900

    Feat: 변수 조정

--- infra/variables.tf
+++ infra/variables.tf
@@ -5,21 +5,24 @@ variable "aws_region" {
 }
 
 variable "project_name" {
-  description = "프로젝트명"
+  description = "프로젝트명, 이벤트 브릿지, 스텝 함수를 이용한 배치 오케스트레이션"
   type        = string
-  default     = "de-ai-16-kakfa-efk"
+  # 프로젝트명 수정
+  default     = "de-ai-16-eb-step-pipeline"
 }
 
 # firehose 이름, firhose->opensearch : iam role name
 variable "firehose_buffer_size" {
   description = "오픈 서치로 전송할때 최대 버퍼 사이즈(MB)"
   type        = number
-  default     = 1
+  # 데이터를 모아서 s3로 전송 (크기 확대)
+  default     = 64
 }
 variable "firehose_buffer_interval" {
   description = "오픈 서치로 전송할때 최대 버퍼 시간(s)"
   type        = number
-  default     = 60
+  # 데이터 전송에 대해 시간 기준 확대
+  default     = 300
 }
 
 # vector -> firhose : iam role name
@@ -39,3 +42,5 @@ variable "tags" {
     Purpose   = "data-engineering-lab"
   }
 }
+
+# 추가되는 리소스에 맞춰 변수 추가
