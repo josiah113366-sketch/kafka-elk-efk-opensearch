f7804fd9cfc880aac634ee1dca4cbe67e339a268
josiah113366-sketch <josiah113366@gmail.com>
2026-08-31 14:28:46 +0900

    Feat: 람다 함수 리소스 구성

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,4 +1,5 @@
-# *.py 파일 -> 압축(zip) -> 람다 함수 등록 시 업로드 -> 관련 정보 조회# *.py 파일 -> 압축(zip) -> 람다 함수 등록시 업로드 -> 관련 정보 조회
+# *.py 파일 -> 압축(zip) -> 람다 함수 등록 시 업로드 -> 관련 정보 조회
+# py 원소스 위치, 차후 압축되면 생성되는 zip 위치 지정 -> 조회 통해서 가져올 수 있게 구성
 data "archive_file" "check_bronze" {
   type        = "zip"
   source_file = "${path.module}/../lambda/check_bronze.py"
@@ -15,4 +16,34 @@ data "archive_file" "quality_check" {
   type        = "zip"
   source_file = "${path.module}/../lambda/quality_check.py"
   output_path = "${path.module}/.quality_check.zip"
+}
+
+# 람다 함수 3개 리소스 생성
+resource "aws_lambda_function" "check_bronze" {
+  # 이름 
+  function_name = 
+  # 업무 -> 권한 -> role 
+  role = 
+  # 파이썬 작동 -> 런타임 환경
+  runtime = 
+  # 엔트리 포인트 (시작점 지정)
+  handler = 
+  # 소스
+  filename = 
+  # 소스 업데이트
+  source_code_hash = 
+  # 작업 최대 시간 
+  timeout = 
+  # 람다 사용할 최대 메모리
+  memory_size = 
+  # 환경 변수
+  environment {
+    
+  }
+}
+resource "aws_lambda_function" "cleanup_gold" {
+  
+}
+resource "aws_lambda_function" "quality_check" {
+  
 }
